1c7ca8bdf5b9556cdec8ceac08c69ec4089f6afb
Aaron Schwartz <aschwartz628@gmail.com>
2016-10-04 21:04:27 -0400

    Server Done

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ var port = process.env.PORT || 8080;
   server.get('/', function(request,response){
       response.sendFile('public/html/index.html', {root:__dirname});
   });
+
+  server.listen(port, function(){
+    console.log('Now listening on port...', port);
+  });
