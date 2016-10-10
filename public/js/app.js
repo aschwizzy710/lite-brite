983821eeb6f8f3c32facfa64bdbef2f729f46c2c
Aaron Schwartz <aschwartz628@gmail.com>
2016-10-10 19:00:27 -0400

    Refactored makeGrid function to take row and column arguments as input

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,23 @@
 (function() {
 
-    var numberOfRows = 40; // number of rows in the grid
-    var numberOfCols = 40; // number of cols in the grid
+    // var numberOfRows = 15; // number of rows in the grid
+    // var numberOfCols = 15; // number of cols in the grid
     var canvas = $('#canvas'); // my placement area - think of paper in drawing
 
 
-    makeGrid();
+    makeGrid(15, 15);
+    clearGrid();
+    makeGrid(30, 30);
     $('.cell').on('click', changeColor);
 
-    function makeGrid(){
+    function clearGrid(){
+      canvas.empty();
+    }
+    function changeColor(event){
+      // just this cell's background
+      $(this).toggleClass('red');
+    }
+    function makeGrid(numberOfRows, numberOfCols){
       //let's make some rows and put them in the body
       for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1){
         var row = $('<tr></tr>');
@@ -20,12 +29,5 @@
         canvas.append(row);
       }
     }
-
-      function changeColor(event){
-        // just this cell's background
-        $(this).toggleClass('red');
-      }
-
-
 
 }());
